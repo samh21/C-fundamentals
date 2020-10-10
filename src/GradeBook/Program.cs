54eb611c4dccf8e05a71f9aa229d6b22dfc88c2f
Sam Henderson <jipo_00@hotmail.com>
2020-10-10 21:15:03 +0100

    initial commit

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            var book = new Book("Sam's Book");
+            book.AddGrade(89.1);
+            book.AddGrade(83.7);
+            book.AddGrade(81.4);
+            var stats = book.GetStatistics();
+            System.Console.WriteLine($"Your highest grade is {stats.High}");
+            System.Console.WriteLine($"Your lowest grade is {stats.Low}");
+            System.Console.WriteLine($"`your average grade is {stats.Average:N1}");
+
+        }
+    }
+}
